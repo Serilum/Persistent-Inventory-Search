2b6c5051f72df6f51ae0846736abaed602dce099
Rick South <rick@natamus.com>
2024-07-28 07:21:26 +0200

    (v1.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/persistentinventorysearch/util/Reference.java
+++ Common/src/main/java/com/natamus/persistentinventorysearch/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.persistentinventorysearch.util;
 public class Reference {
 	public static final String MOD_ID = "persistentinventorysearch";
 	public static final String NAME = "Persistent Inventory Search";
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "1.5";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
